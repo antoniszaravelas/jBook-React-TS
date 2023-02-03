69ec1669803557d5560fac1a543211f3fc4132c5
Antonios Zaravelas <antonis@Antonioss-Air.fritz.box>
2023-02-03 17:46:10 +0100

    conditional toggling

---
+++ jbook/src/components/text-editor.tsx
@@ -0,0 +1,42 @@
+import MDEditor from "@uiw/react-md-editor";
+import { useState, useEffect, useRef } from "react";
+
+const TextEditor: React.FC = ({}) => {
+  const [value, setValue] = useState("**Hello world!!!**");
+  const [editing, setEditing] = useState(false);
+  const editorRef = useRef<any>();
+
+  useEffect(() => {
+    // whenever the user clicks in our document
+    const listener = (event: MouseEvent) => {
+      if (
+        !(
+          editorRef.current &&
+          event.target &&
+          editorRef.current.contains(event.target as Node)
+        )
+      )
+        setEditing(false);
+    };
+    document.addEventListener("click", listener, { capture: true });
+
+    return () =>
+      document.removeEventListener("click", listener, { capture: true });
+  }, []);
+
+  if (editing) {
+    return (
+      <div ref={editorRef}>
+        <MDEditor />
+      </div>
+    );
+  }
+
+  return (
+    <div onClick={() => setEditing(true)} className="container">
+      <MDEditor.Markdown source={value} style={{ whiteSpace: "pre-wrap" }} />
+    </div>
+  );
+};
+
+export default TextEditor;
--- jbook/src/index.tsx
+++ jbook/src/index.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import CodeCell from "./components/code-cell";
 import "bulmaswatch/superhero/bulmaswatch.min.css";
+import TextEditor from "./components/text-editor";
 
 const App = () => {
-  return <CodeCell />;
+  // return <CodeCell />;
+  return <TextEditor />;
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
