078f2b4ead05c338bd302c6d1cc2c5e71495014a
Antonios Zaravelas <antonis@Antonioss-Air.fritz.box>
2023-02-05 18:39:14 +0100

    fixing the buttons

--- jbook/src/components/action-bar.tsx
+++ jbook/src/components/action-bar.tsx
@@ -1,5 +1,31 @@
-const ActionBar: React.FC = () => {
-  return <div>hi</div>;
+import { useActions } from "../hooks/useActions";
+import {
+  faArrowDown,
+  faArrowUp,
+  faTrashCan,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./action-bar.css";
+
+interface ActionBarProps {
+  id: string;
+}
+
+const ActionBar: React.FC<ActionBarProps> = ({ id }) => {
+  const { moveCell, deleteCell } = useActions();
+
+  return (
+    <span className="action-bar">
+      <FontAwesomeIcon onClick={() => moveCell(id, "up")} icon={faArrowUp} />
+
+      <FontAwesomeIcon
+        onClick={() => moveCell(id, "down")}
+        icon={faArrowDown}
+      />
+
+      <FontAwesomeIcon onClick={() => deleteCell(id)} icon={faTrashCan} />
+    </span>
+  );
 };
 
 export default ActionBar;
