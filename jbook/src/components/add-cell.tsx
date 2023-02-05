32b93ed8d9f048c7b2e030cd4afb48c620c37040
Antonios Zaravelas <antonis@Antonioss-Air.fritz.box>
2023-02-05 19:43:53 +0100

    finishing react with redux

--- jbook/src/components/add-cell.tsx
+++ jbook/src/components/add-cell.tsx
@@ -1,5 +1,7 @@
 import "./add-cell.css";
 import { useActions } from "../hooks/useActions";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 interface AddCellProps {
   // because we will need to put the cell before the cell
@@ -10,9 +12,19 @@ const AddCell: React.FC<AddCellProps> = ({ nextCellId }) => {
   const { insertCellBefore } = useActions();
 
   return (
-    <div>
-      <button onClick={() => insertCellBefore(nextCellId, "code")}>Code</button>
-      <button onClick={() => insertCellBefore(nextCellId, "text")}>Text</button>
+    <div className="add-cell">
+      <span></span>
+      <div>
+        <button onClick={() => insertCellBefore(nextCellId, "code")}>
+          <FontAwesomeIcon icon={faPlus} /> Code{"  "}
+        </button>
+
+        <button onClick={() => insertCellBefore(nextCellId, "text")}>
+          {"  "}
+          <FontAwesomeIcon icon={faPlus} />
+          Text
+        </button>
+      </div>
     </div>
   );
 };
